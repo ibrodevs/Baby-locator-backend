4c67c8ae177157b49bd4466585f4770f24c0a58d
ibrodevs <ibroabdraimov20@gmail.com>
2026-04-30 13:26:48 +0600

    safd

--- config/settings.py
+++ config/settings.py
@@ -67,9 +67,13 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'babydb',
+        'USER': 'babyuser',
+        'PASSWORD': 'StrongPassword123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
